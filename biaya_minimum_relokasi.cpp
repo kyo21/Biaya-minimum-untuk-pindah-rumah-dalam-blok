d96b21f7f766e9c3501a55262894467e09a62422
kyo21 <kussa.laksana@gmail.com>
2024-01-15 23:30:30 +0800

    adding file cpp and instructions
    adding file cpp and instructions

---
+++ biaya_minimum_relokasi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <iterator>
+
+using namespace std;
+
+map<int, int> cost;
+map<int, int> blok;
+
+void blok_biaya_terendah(map<int,int> blok){
+//CODE
+
+
+}
+
+int main(){
+
+//rule pindah rumah
+cost[1]=1;
+cost[2]=0;
+
+return 0;
+}
